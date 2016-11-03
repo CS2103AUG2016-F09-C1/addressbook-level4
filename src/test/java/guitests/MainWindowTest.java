d758386831fd0d7a5df13b6ad0cafce381617b39
bernardtankx <bernardtankx@gmail.com>
2016-11-03 19:31:24 +0800

    mainwindowtest

--- src/test/java/guitests/MainWindowTest.java
+++ src/test/java/guitests/MainWindowTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 //@@author A0146840E
-public class MaximizedWindowTest extends TaskListGuiTest {
+public class MainWindowTest extends TaskListGuiTest {
 
     @Test
     public void mainMenu_maximizedWindow() {
